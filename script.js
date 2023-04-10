2ece2ab3ecb22075ddd16ed86fc60cb26a7e2748
Novo <120156959+Novo1999@users.noreply.github.com>
2023-04-10 16:08:58 +0600

    logic done

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const Choices = ['Rock', 'Paper', 'Scissors'];
 let computerChoice;
-let myChoice = document.querySelector('#myChoice').innerHTML;
 function randomChoice() {
     let randomNumber = Math.floor(Math.random() * Choices.length);
     computerChoice = Choices[randomNumber];
@@ -13,8 +12,8 @@ function pcPick(id){
             randomChoice();
             const clicked = e.currentTarget.classList;
             if(clicked.contains(id)){
-                newString = `Computer Picked : ${computerChoice}`;
-                document.querySelector('#pcChoice').innerHTML = newString;
+                newString = `${computerChoice}`;
+                document.querySelector('#pcPick').innerHTML = newString;
                 myPick(clicked);
             }
         })
@@ -22,20 +21,58 @@ function pcPick(id){
 }
 function myPick(clicked){
     if(clicked.contains('rock-color')){
-        document.querySelector('#myChoice').innerHTML = `You Picked : Rock`;
+        document.querySelector('#myPick').innerHTML = `Rock`;
     }
     if(clicked.contains('paper-color')){
-        document.querySelector('#myChoice').innerHTML = `You Picked : Paper`;
+        document.querySelector('#myPick').innerHTML = `Paper`;
     }
     if(clicked.contains('scissor-color')){
-        document.querySelector('#myChoice').innerHTML = `You Picked : Scissors`;
+        document.querySelector('#myPick').innerHTML = `Scissors`;
     }
-    
+}
+pcPick('rock-color');
+pcPick('paper-color');
+pcPick('scissor-color');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', ()=>{
+        pcWin('Rock', 'Paper');
+        pcWin('Paper', 'Scissors');
+        pcWin('Scissors', 'Rock');
+        playerWin('Paper', 'Rock');
+        playerWin('Rock', 'Scissors');
+        playerWin('Scissors', 'Paper');
+    })
+})
+function pcWin(id1, id2){
+    const myPick = document.querySelector('#myPick').innerHTML;
+    let value2 = document.querySelector('#pcScore').innerHTML;
+    const pcPick = document.querySelector('#pcPick').innerHTML;
+    if(myPick == id1 && pcPick == id2){
+        value2++;
+        document.querySelector('#pcScore').innerHTML = value2;
+    }
+}
+function playerWin(id1,id2){
+    const myPick = document.querySelector('#myPick').innerHTML;
+    let value1 = document.querySelector('#myScore').innerHTML;
+    const pcPick = document.querySelector('#pcPick').innerHTML;
+    if(myPick == id1 && pcPick == id2){
+        value1++;
+        document.querySelector('#myScore').innerHTML = value1;
+    }
 }
 
-const rock = pcPick('rock-color');
-const paper = pcPick('paper-color');
-const scissor = pcPick('scissor-color');
+// if I am rock and PC is paper,PC wins
+// if I am paper and PC is scissors,PC wins
+// if I am scissors and PC is rock,PC wins
+// if I am paper and PC is rock,I win
+// if I am rock and PC is scissors,I win
+// if I am scissors and PC is paper,I win
+
+
+
+
 
 
 
